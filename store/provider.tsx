aac4264964af257c683db49a71efbd6a76f08a69
anas <ktmanas2020@gmail.com>
2024-09-26 09:58:16 +0530

    provider is a component

--- store/provider.tsx
+++ store/provider.tsx
@@ -15,8 +15,10 @@
 // src/store/Providers.tsx
 
 'use client';
-import { Provider } from 'react-redux'; 
+ 
 import { store } from './store'; 
+
+import { Provider } from 'react-redux'; 
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
