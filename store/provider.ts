f18a5a9d0913be0b74ddfc7a8c95df30d9b4258a
Priyanandan-4 <priyanandanofficialwork@gmail.com>
2024-09-26 09:46:22 +0530

    provider

--- store/provider.ts
+++ store/provider.ts
@@ -11,3 +11,15 @@
 // };
 
 // export default Providers;
+
+// src/store/Providers.tsx
+
+'use client';
+import { Provider } from 'react-redux'; 
+import { store } from './store'; 
+
+const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default Providers;
